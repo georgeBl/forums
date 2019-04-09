2ff7a69074eed7775de47dc2daf485da8033ff61
georgeBl <blanarugeorge96@gmail.com>
2019-04-09 04:19:36 +0100

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -41,8 +41,8 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
-      template: '../public/index.html',
-      favicon: '../public/favicon.ico'
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
     })
   ],
   performance: {
